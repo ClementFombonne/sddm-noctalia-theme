185b9ad4499d615b711141799f649fd5040875e1
clement fombonne <clement.fombonne@proton.me>
2026-01-20 13:02:52 +0100

    add nixos flake

--- flake.nix
+++ flake.nix
@@ -1,50 +1,25 @@
 {
-  description = "QML Development Environment with working LSP";
+  description = "Noctalia SDDM Theme";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux"; # Change to aarch64-darwin for Mac, etc.
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+    { self, nixpkgs, ... }:
     {
-      devShells.${system}.default = pkgs.mkShell {
-        # 1. Packages available in the shell
-        packages = with pkgs; [
-          # Build tools
-          cmake
-          ninja
-          pkg-config
+      nixosModules.default = import ./nix/module.nix;
 
-          # Qt6 full suite (includes qmlls, qmlformat)
-          qt6.qtbase
-          qt6.qtdeclarative
-          qt6.qt5compat
-          qt6.qttools
-          qt6.qtquick3d # Add other Qt modules as needed
-        ];
-
-        # 2. Automatically Setup Environment Variables
-        # This hook automatically calculates QML_IMPORT_PATH and QT_PLUGIN_PATH
-        # based on the inputs listed above.
-        nativeBuildInputs = [
-          pkgs.qt6.wrapQtAppsHook
-        ];
-
-        # 3. Explicitly export variables for the editor
-        # While the hook helps apps run, we sometimes need to explicitly
-        # export these so Neovim/qmlls inherits them.
-        shellHook = ''
-          export QML_IMPORT_PATH="${pkgs.qt6.qtdeclarative}/lib/qt-6/qml:${pkgs.qt6.qtquick3d}/lib/qt-6/qml"
-          export QT_PLUGIN_PATH="${pkgs.qt6.qtbase}/lib/qt-6/plugins"
-
-          echo "🚀 QML Environment Loaded"
-          echo "QML_IMPORT_PATH is set to: $QML_IMPORT_PATH"
-        '';
-      };
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage (
+        { stdenv, lib }:
+        stdenv.mkDerivation {
+          name = "noctalia-sddm-base";
+          src = lib.cleanSource ./.;
+          installPhase = ''
+            mkdir -p $out/share/sddm/themes/noctalia-sddm
+            cp -r * $out/share/sddm/themes/noctalia-sddm
+          '';
+        }
+      ) { };
     };
 }
